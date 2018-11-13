c59bf0fced789367e4c915f755322fd08985835d
Adrian Steppat <adrian.steppat@outlook.de>
2018-11-13 12:18:55 +0100

    Add routes for get, update and delete

--- application/artefacts/artefacts_controller.py
+++ application/artefacts/artefacts_controller.py
@@ -1,10 +1,24 @@
 from flask import current_app
 import uuid
 import datetime
+from elasticsearch.exceptions import NotFoundError
+
+def show(params): 
+    if not current_app.es:
+        return {"error":"search engine not available"}, 503
+    try:
+        result = current_app.es.get(
+            index="artefacts",
+            doc_type=params["type"],
+            id=params["id"]
+        )
+        return result, 200
+    except NotFoundError:
+        return {"error":"not found"}, 404
 
 def index(params):
     if not current_app.es:
-        return {"error":"search engine not available"}
+        return {"error":"search engine not available"}, 503
     
     date_range = {
         "gte": params["date_range"]["start_date"],
@@ -32,18 +46,50 @@ def index(params):
                 }   
             }
         })
-    return result["hits"]["hits"]
+    return result["hits"]["hits"], 200
 
 def create(params):
     if not current_app.es:
-        return {"error":"search engine not available"}
+        return {"error":"search engine not available"}, 503
     date = datetime.datetime.now().isoformat()
-    return current_app.es.index(
+    return current_app.es.create(
         index="artefact", 
         doc_type=params["type"], 
         id=params["id"], 
         body={
-            "text": params["tags"], 
+            "tags": params["tags"], 
             "file_url": params["file_url"],
             "created_at": date
-        })
+        }), 201
+
+def update(params):
+    if not current_app.es:
+        return {"error":"search engine not available"}, 503
+    
+    update_params = {}
+    if "tags" in params: update_params["tags"] = params["tags"] 
+    if "image_url" in params: update_params["file_url"] = params["file_url"]
+
+    try:
+        current_app.es.update(
+            index="artefact",
+            doc_type=params["type"],
+            id=params["id"],
+            body={
+                "doc" : update_params
+            })
+        return '', 204
+    except NotFoundError:
+        return {"error":"not found"}, 404     
+
+def delete(params):
+    if not current_app.es:
+        return {"error":"search engine not available"}, 503
+    try:
+        current_app.es.delete(
+            index="artefact",
+            doc_type=params["type"],
+            id=params["id"])
+        return '', 204
+    except NotFoundError:
+        return {"error":"not found"}, 404     
