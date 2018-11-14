635423707a49dfea05601557fa16a4cce6484f6a
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2018-11-14 13:40:41 +0100

    Setup CircleCI with continuous integration and deployment

--- app.py
+++ app.py
@@ -2,17 +2,10 @@ import os
 import signal
 from flask import Flask
 from flask import jsonify
-from application import misc
 
 app = Flask(__name__)
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
-
-@app.route("/")
-def api_status():
-    return jsonify({
-        "status": "on" if misc.api_status() else "off"
-        })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
--- application/misc.py
+++
@@ -1,5 +0,0 @@
-def api_status():
-    return True
-
-if __name__ == "__main__":
-    print(api_status())
