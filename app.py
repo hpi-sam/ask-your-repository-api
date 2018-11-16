5333113e9846814746a7b00610250d1491b1b2bd
Adrian Steppat <adrian.steppat@web.de>
2018-11-16 10:37:27 +0100

    Add artefact-service.ini for uWSGI support.

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ from application import create_app
 # Call the Application Factory function to construct a Flask application instance
 # using the standard configuration defined in /instance/flask.cfg
 app = create_app('development_config.cfg')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
