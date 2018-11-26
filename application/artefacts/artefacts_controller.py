231aae73beecfec0fe86c31dd5854b52f4189119
Adrian Steppat <adrian.steppat@web.de>
2018-11-26 15:57:54 +0100

    Remove type from get with id, update and delete

--- application/artefacts/artefacts_controller.py
+++ application/artefacts/artefacts_controller.py
@@ -16,7 +16,7 @@ def show(params):
     try:
         result = current_app.es.get(
             index="artefact",
-            doc_type=params["type"],
+            doc_type="_all",
             id=params["id"]
         )
         return result, 200
@@ -36,9 +36,10 @@ def index(params):
     if "end_date" in params:
         date_range["lte"] = params["end_date"]
 
+    artefact_types = params.get("types", "")
     result = current_app.es.search(
         index="artefact",
-        doc_type=params["type"],
+        doc_type=artefact_types,
         body=search_body_helper(params.get("search", ""), date_range))
 
     return {"results": result["hits"]["hits"]}, 200
@@ -51,6 +52,7 @@ def create(params):
         return {"error": "search engine not available"}, 503
 
     date = datetime.datetime.now().isoformat()
+    artefact_type = params.get("type", "image")
     body = {
         "tags": params["tags"],
         "file_url": params["file_url"],
@@ -60,7 +62,7 @@ def create(params):
     try:
         result = current_app.es.create(
             index="artefact",
-            doc_type=params["type"],
+            doc_type=artefact_type,
             id=params["id"],
             body=body)
         result["_source"] = body
@@ -78,20 +80,26 @@ def update(params):
     update_params = {}
     if "tags" in params:
         update_params["tags"] = params["tags"]
-    if "image_url" in params:
+    if "file_url" in params:
         update_params["file_url"] = params["file_url"]
 
     try:
-        current_app.es.update(
+        result = current_app.es.get(
             index="artefact",
-            doc_type=params["type"],
-            id=params["id"],
-            body={
-                "doc": update_params
-            })
-        return '', 204
+            doc_type="_all",
+            id=params["id"]
+        )
     except NotFoundError:
         return {"error": "not found"}, 404
+
+    current_app.es.update(
+        index="artefact",
+        doc_type=result["_type"],
+        id=params["id"],
+        body={
+            "doc": update_params
+        })
+    return '', 204
 
 
 def delete(params):
@@ -101,14 +109,19 @@ def delete(params):
         return {"error": "search engine not available"}, 503
 
     try:
-        current_app.es.delete(
+        result = current_app.es.get(
             index="artefact",
-            doc_type=params["type"],
-            id=params["id"])
-        return '', 204
+            doc_type="_all",
+            id=params["id"]
+        )
     except NotFoundError:
         return {"error": "not found"}, 404
 
+    current_app.es.delete(
+        index="artefact",
+        doc_type=result["_type"],
+        id=params["id"])
+    return '', 204
 
 def search_body_helper(search, daterange):
     """ Defines a common body for search function """
