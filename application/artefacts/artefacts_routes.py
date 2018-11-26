bf2527751218e7f512e6aad973f1c24e40023f70
Adrian Steppat <adrian.steppat@web.de>
2018-11-26 13:45:05 +0100

    Edit routes for artefacts. Remove /image.

--- application/artefacts/artefacts_routes.py
+++ application/artefacts/artefacts_routes.py
@@ -6,46 +6,51 @@ from . import artefact_blueprint
 from . import artefacts_controller
 
 
-@artefact_blueprint.route('/artefacts/<string:artefact_type>/<string:artefact_id>', methods=['GET'])
-def show(artefact_type, artefact_id):
+@artefact_blueprint.route('/artefacts/<string:artefact_id>', methods=['GET'])
+def show(artefact_id):
     "Route to get a single artefact"
+    artefact_type = request.args["type"] if "type" in request.args else "image"
     params = {"type": artefact_type, "id": artefact_id}
     response, status = artefacts_controller.show(params)
     return make_response(jsonify(response), status)
 
 
-@artefact_blueprint.route('/artefacts/<string:artefact_type>', methods=['GET'])
-def index(artefact_type):
+@artefact_blueprint.route('/artefacts', methods=['GET'])
+def index():
     "Route to get several artefacts with filters"
-    params = request.json or {}
-    params["type"] = artefact_type
+    params = dict(request.args) or {}
+    if not "type" in params:
+        params["type"] = "image"
     response, status = artefacts_controller.index(params)
     return make_response(jsonify(response), status)
 
 
-@artefact_blueprint.route('/artefacts/<string:artefact_type>', methods=['POST'])
-def create(artefact_type):
+@artefact_blueprint.route('/artefacts', methods=['POST'])
+def create():
     "Route to create an artefact"
     params = request.json
-    params["type"] = artefact_type
+    if not "type" in params:
+        params["type"] = "image"
     response, status = artefacts_controller.create(params)
     return make_response(jsonify(response), status)
 
 
-@artefact_blueprint.route('/artefacts/<string:artefact_type>/<string:artefact_id>', methods=['PUT'])
-def update(artefact_type, artefact_id):
+@artefact_blueprint.route('/artefacts/<string:artefact_id>', methods=['PUT'])
+def update(artefact_id):
     "Route to update an artefact"
     params = request.json
-    params["type"] = artefact_type
+    if not "type" in params:
+        params["type"] = "image"
     params["id"] = artefact_id
     response, status = artefacts_controller.update(params)
     return make_response(jsonify(response), status)
 
 
-@artefact_blueprint.route('/artefacts/<string:artefact_type>/<string:artefact_id>',
+@artefact_blueprint.route('/artefacts/<string:artefact_id>',
                           methods=['DELETE'])
-def delete(artefact_type, artefact_id):
+def delete(artefact_id):
     "Route to delete an artefact"
+    artefact_type = request.args["type"] if "type" in request.args else "image"
     params = {"type": artefact_type, "id": artefact_id}
     response, status = artefacts_controller.delete(params)
     return make_response(jsonify(response), status)
