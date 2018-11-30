02ed26393da6082238866d96ec48d719068bd4d4
Jascha Beste <bestejascha@gmail.com>
2018-11-30 14:47:39 +0100

    Add doublex and doublex-expects for testing.

--- specs/artifacts_spec.py
+++ specs/artifacts_spec.py
@@ -1,6 +1,8 @@
 from flask import current_app
 from mamba import description, before, after, it
 from expects import expect, equal
+from doublex_expects import *
+from doublex import Mock
 
 import sys
 sys.path.insert(0, 'specs')
@@ -9,17 +11,36 @@ from specs.spec_helpers import Context
 from specs.mock_elasticsearch import ElasticMock
 
 
-with description('artifacts show') as self:
-    with before.each:
+with description('/artifacts') as self:
+
+    with before.each: 
         self.context = Context()
         current_app.es = ElasticMock()
-        current_app.es.mock(function_name="get",
-                            return_value={"_source":{"class_diagram.png":""}})
 
     with after.each:
-        self.context.delete()
+            self.context.delete()
 
-    with it('returns a 200 status code'):
-        response = self.context.client().get("/artifacts/1")
+    with description('GET'):
+        with before.each:
+            current_app.es.mock(function_name="get",
+                            return_value={"_source":{"class_diagram.png":""}})
+            self.response = self.context.client().get("/artifacts/1")
+            
 
-        expect(response.status_code).to(equal(200))
+        with it('returns a 200 status code'):
+            expect(self.response.status_code).to(equal(200))
+
+    with description('POST'):
+        with before.each:
+            current_app.es.mock(function_name="create",
+                            return_value={})
+
+            self.response = self.context.client().post("/artifacts",  json={
+                "id": "asdf",
+                "type": "image",
+                "tags": [],
+                "file_url": "asdf"})
+            
+
+        with it('returns a 201 status code'):
+            expect(self.response.status_code).to(equal(201))
