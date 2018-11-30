ca83cbb353401d08e3c673f817bb7a81469180d3
Jascha Beste <bestejascha@gmail.com>
2018-11-30 17:10:30 +0100

    Change to use doublex instead of a custom ElasticMock

--- specs/artifacts_spec.py
+++ specs/artifacts_spec.py
@@ -4,45 +4,40 @@ import sys
 from flask import current_app
 from mamba import description, before, after, it
 from expects import expect, equal
-#from doublex_expects import *
-#from doublex import Mock
+from doublex_expects import *
+from doublex import Mock, Stub
 
 sys.path.insert(0, 'specs')
 
 #pylint: disable=wrong-import-position
-from specs.spec_helpers import Context, ElasticMock
+from specs.spec_helpers import Context
 #pylint: enable=wrong-import-position
 
 with description('/artifacts') as self:
 
     with before.each:
         self.context = Context()
-        current_app.es = ElasticMock()
+        current_app.es = Stub()
 
     with after.each:
         self.context.delete()
 
     with description('GET'):
         with before.each:
-            current_app.es.mock(function_name="get",
-                                return_value={"_source":{"class_diagram.png":""}})
+            with Mock() as elastic_mock:
+                 elastic_mock.get(doc_type='_all', id='1', index='artifact').returns({"_source":{"class_diagram.png":""}})
+            current_app.es = elastic_mock
             self.response = self.context.client().get("/artifacts/1")
-
 
         with it('returns a 200 status code'):
             expect(self.response.status_code).to(equal(200))
 
     with description('POST'):
         with before.each:
-            current_app.es.mock(function_name="create",
-                                return_value={})
+            with Mock() as elastic_mock:
+                 elastic_mock.get(doc_type='_all', id='1', index='artifact').returns({"_source":{"class_diagram.png":""}})
+            current_app.es = elastic_mock
+            self.response = self.context.client().get("/artifacts/1")
 
-            self.response = self.context.client().post("/artifacts",  json={
-                "id": "asdf",
-                "type": "image",
-                "tags": [],
-                "file_url": "asdf"})
-
-
-        with it('returns a 201 status code'):
-            expect(self.response.status_code).to(equal(201))
+        with it('returns a 200 status code'):
+            expect(self.response.status_code).to(equal(200))
