9a8b158707bc29a2da9a5c763ff652c64c552b95
Adrian Steppat <adrian.steppat@web.de>
2018-12-01 14:17:31 +0100

    Remove leftovers from blueprints

--- application/__init__.py
+++ application/__init__.py
@@ -21,7 +21,6 @@ def create_app(config_filename=None):
     app.es = (Elasticsearch(app.config['ELASTICSEARCH_URL'])
               if app.config['ELASTICSEARCH_URL'] else None)
     app.register_blueprint(api_bp)
-    #register_blueprints(app)
     return app
 
 def add_resources(api):
@@ -30,13 +29,3 @@ def add_resources(api):
     api.add_resource(HomeResources, '/')
     api.add_resource(ArtifactsResource, '/artifacts')
     api.add_resource(ArtifactResource, '/artifacts/<artifact_id>')
-
-def register_blueprints(app):
-    """
-    Register all new blueprints here.
-    Blueprints are components of the app that belong together e.g. artefacts
-    """
-    from application.artifacts import artifact_blueprint
-    from application.home import home_blueprint
-    app.register_blueprint(artifact_blueprint)
-    app.register_blueprint(home_blueprint)
