40fde149954cfd65a36fbf9b5a0853ce6a0937a4
Adrian Steppat <adrian.steppat@web.de>
2018-12-01 02:33:17 +0100

    Refactor app to use flask_restful

--- utils/setup_elasticsearch.py
+++ utils/setup_elasticsearch.py
@@ -6,13 +6,21 @@ load_dotenv()
 
 def setup_indices():
     es = Elasticsearch(os.environ.get('ES_URL'))
-    es.indices.create(index="artefact", ignore=400, body= {
+    es.indices.create(index="artifact", ignore=400, body= {
             "mappings": {
-                "image": { 
-                    "properties": { 
-                        "tags": { "type": "text"  }, 
-                        "image_url": { "type": "text"  },
+                "image": {
+                    "properties": {
+                        "tags": { "type": "text"  },
+                        "file_url": { "type": "text"  },
+                        "file_date":  {
+                            "type":   "date",
+                            "format": "strict_date_optional_time||epoch_millis"
+                        },
                         "created_at":  {
+                            "type":   "date",
+                            "format": "strict_date_optional_time||epoch_millis"
+                        },
+                        "updated_at":  {
                             "type":   "date",
                             "format": "strict_date_optional_time||epoch_millis"
                         }
