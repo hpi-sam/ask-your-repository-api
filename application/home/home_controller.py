9cb34b82a5f7677ad87e28382e29152efb94b057
Adrian Steppat <adrian.steppat@web.de>
2018-12-01 14:15:12 +0100

    Change routing for home_controller

--- application/home/home_controller.py
+++ application/home/home_controller.py
@@ -3,8 +3,12 @@ Handles all logic of the home api
 """
 
 from flask import current_app
+from flask_restful import Resource
 
-def index():
-    "Create a json description that is shown when connecting to the service without params"
-    database_status = "on" if current_app.es else "off"
-    return {"service name":"artefact service", "database status": database_status}
+class HomeResources(Resource):
+    """ Defines Routes on collection """
+
+    def get(self):
+        "Create a json description that is shown when connecting to the service without params"
+        database_status = "on" if current_app.es else "off"
+        return {"service name":"artefact service", "database status": database_status}
