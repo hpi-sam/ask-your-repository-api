d53116cdd3f740af567baefbad4c548c0d57d12c
Jascha Beste <bestejascha@gmail.com>
2018-12-03 04:59:52 +0100

    Fix testing with mamba.

--- specs/artifacts_spec.py
+++ specs/artifacts_spec.py
@@ -4,7 +4,7 @@ import sys
 from flask import current_app
 from mamba import description, before, after, it
 from expects import expect, equal
-from doublex import Mock, Stub
+from doublex import Mock, Stub, ANY_ARG
 # pylint: disable=wrong-import-position
 from specs.spec_helpers import Context
 # pylint: enable=wrong-import-position
@@ -21,28 +21,53 @@ with description('/artifacts') as self:
     with after.each:
         self.context.delete()
 
-    with description('GET'):
-        with before.each:
-            with Mock() as elastic_mock:
-                elastic_mock.get(
-                    doc_type='_all', id='1',
-                    index='artifact').returns(
-                        {"_source": {"class_diagram.png": ""}})
-            current_app.es = elastic_mock
-            self.response = self.context.client().get("/artifacts/1")
+    with description('/'):
+        with description('GET'):
+            with before.each:
+                with Mock() as elastic_mock:
+                    elastic_mock.search(ANY_ARG).returns(
+                        {"hits": {"hits": []}})
 
-        with it('returns a 200 status code'):
-            expect(self.response.status_code).to(equal(200))
+                current_app.es = elastic_mock
+                self.response = self.context.client().get("/artifacts")
+            with it('returns a 200 status code'):
+                expect(self.response.status_code).to(equal(200))
 
-    with description('POST'):
-        with before.each:
-            with Mock() as elastic_mock:
-                elastic_mock.get(
-                    doc_type='_all', id='1',
-                    index='artifact').returns(
-                        {"_source": {"class_diagram.png": ""}})
-            current_app.es = elastic_mock
-            self.response = self.context.client().get("/artifacts/1")
+    with description('/1'):
+        with description('GET'):
+            with before.each:
+                with Mock() as elastic_mock:
+                    elastic_mock.get(
+                        doc_type='_all', id='1',
+                        index='artifact').returns(
+                            {"_id": "asdf",
+                             "_type": "image",
+                             "_source": {"tags": ["class_diagram.png", ""],
+                                         "created_at": "today",
+                                         "updated_at": "today",
+                                         "file_url": "asdf",
+                                         "file_date": "today"}})
+                current_app.es = elastic_mock
+                self.response = self.context.client().get("/artifacts/1")
 
-        with it('returns a 200 status code'):
-            expect(self.response.status_code).to(equal(200))
+            with it('returns a 200 status code'):
+                expect(self.response.status_code).to(equal(200))
+
+        with description('POST'):
+            with before.each:
+                with Mock() as elastic_mock:
+                    elastic_mock.get(
+                        doc_type='_all', id='1',
+                        index='artifact').returns(
+                            {"_id": "asdf",
+                             "_type": "image",
+                             "_source": {"tags": ["class_diagram.png", ""],
+                                         "created_at": "today",
+                                         "updated_at": "today",
+                                         "file_url": "asdf",
+                                         "file_date": "today"}})
+                current_app.es = elastic_mock
+                self.response = self.context.client().get("/artifacts/1")
+
+            with it('returns a 200 status code'):
+                expect(self.response.status_code).to(equal(200))
