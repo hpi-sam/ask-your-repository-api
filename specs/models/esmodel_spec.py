ad50cdedd3d925954028e0e79fdc54abd0abbf9b
Adrian Steppat <adrian.steppat@web.de>
2018-12-04 17:37:32 +0100

    test esmodel

---
+++ specs/models/esmodel_spec.py
@@ -0,0 +1,40 @@
+""" Tests ESModel """
+
+import sys
+from flask import current_app
+from mamba import description, before, after, it
+from expects import expect, equal, raise_error
+from application.models.esmodel import ESModel
+from application.errors import NotInitialized
+from doublex import Mock, Stub, ANY_ARG
+# pylint: disable=wrong-import-position
+from specs.spec_helpers import Context
+# pylint: enable=wrong-import-position
+
+sys.path.insert(0, 'specs')
+
+with description('ESModel') as self:
+
+    with before.each:
+        self.context = Context()
+        current_app.es = Stub()
+
+    with after.each:
+        self.context.delete()
+
+    with description('update'):
+        with before.each:
+            with Mock() as elastic_mock:
+                elastic_mock.update(ANY_ARG)
+                elastic_mock.index(ANY_ARG)
+
+            current_app.es = elastic_mock
+            self.es_model = ESModel({"type": "image"})
+            self.es_model.index = "artifact"
+
+        with it("raises NotInitialized error if it wasn't saved before"):
+            expect(lambda: self.es_model.update({})).to(raise_error(NotInitialized))
+
+        with it("returns True if it was saved before"):
+            self.es_model.save()
+            expect(self.es_model.update({})).to(equal(True))
