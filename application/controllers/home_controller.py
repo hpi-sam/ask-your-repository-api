c9e335c03cbd840deeb4af13110176a54f6bb3b5
Adrian Steppat <adrian.steppat@web.de>
2018-12-12 12:34:52 +0100

    Remove test logging outputs

--- application/controllers/home_controller.py
+++ application/controllers/home_controller.py
@@ -13,12 +13,6 @@ class HomeController(ApplicationController):
         """
         Create a json description that is shown when connecting to the service without params
         """
-        current_app.logger.debug("this is a DEBUG message")
-        current_app.logger.info("this is an INFO message")
-        current_app.logger.warning("this is a WARNING message")
-        current_app.logger.error("this is an ERROR message")
-        current_app.logger.critical("this is a CRITICAL message")
-
         database_status = "on" if current_app.es else "off"
         return {"service name": "artefact service",
                 "database status": database_status}
