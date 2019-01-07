1c911d5f695430da7005ace493cecdbf2cb760a9
arne-z <arne.zerndt@student.hpi.de>
2019-01-07 20:37:13 +0100

    Add basic team routes. #32

--- application/controllers/teams_controller.py
+++ application/controllers/teams_controller.py
@@ -10,13 +10,24 @@ from .application_controller import ApplicationController
 from application.models.team import Team
 from application.base import respond_with
 
+
 class TeamsController(ApplicationController):
-    """ Controller for Artifacts """
+    """ Controller for teams """
+
+    def show(self, object_id):
+        try:
+            return respond_with(Team.find_by(force=True, id_=object_id)), 200
+        except NotFound:
+            return {"error": "not found"}, 404
 
     @use_args(teams_validator.get_args())
     def index(self, params):
-        "Logic for querying several teams"
-
+        """Logic for querying several teams"""
         teams = Team.all()
-
         return {"teams": respond_with(teams)}, 200
+
+    @use_args(teams_validator.create_args())
+    def create(self, params):
+        """Logic for creating a team"""
+        team = Team.create(name=params["name"])
+        return respond_with(team), 200
--- specs/requests/teams_spec.py
+++ specs/requests/teams_spec.py
@@ -2,7 +2,7 @@ import sys
 from io import BytesIO
 from flask import current_app
 from mamba import shared_context, included_context, description, context, before, after, it
-from expects import expect, equal, have_key
+from expects import *
 from hamcrest import matches_regexp
 from elasticsearch.exceptions import NotFoundError
 from doublex import Mock, Stub, ANY_ARG
@@ -14,12 +14,13 @@ from specs.factories.date_fixture import get_date, date_regex
 
 sys.path.insert(0, 'specs')
 
-
 with description('/teams') as self:
-
     with before.each:
         self.context = Context()
         current_app.es = Stub()
+
+    with after.each:
+        current_app.graph.delete_all()
 
     with description('GET'):
         with before.each:
@@ -28,6 +29,59 @@ with description('/teams') as self:
             self.response = self.context.client().get("/teams")
 
         with it('responds with all teams'):
-            print(self.response.json)
+            expect(self.response.json).to(have_key("teams"))
+            expect(self.response.json["teams"]).to(have_len(2))
+            expect(self.response.json["teams"]).to(contain_only(
+                have_key("name", "Blue"),
+                have_key("name", "Red")
+            ))
 
+    with description('POST'):
+        with description('valid request'):
+            with before.each:
+                self.response = self.context.client().post(
+                    "/teams",
+                    content_type='multipart/form-data',
+                    data={
+                        "name": "My Team"
+                    })
+            with it('responds with 200'):
+                expect(self.response.status_code).to(equal(200))
+            with it('responds with correct team'):
+                expect(self.response.json).to(have_key("name", "My Team"))
+                expect(self.response.json).to(have_key("id_"))
+            with it('saves the team'):
+                expect(Team.exists(id_=self.response.json["id_"], name="My Name")).to(be(True))
 
+        with description('invalid request'):
+            with before.each:
+                self.response = self.context.client().post(
+                    "/teams",
+                    content_type='application/json',
+                    data={
+                        "name": ""
+                    })
+            with it('declines empty name'):
+                expect(self.response.status_code).to(equal(422))
+            with it('does not save invalid teams'):
+                expect(Team.all()).to(be_empty)
+
+    with description('/:id'):
+        with description('GET'):
+            with description('valid id'):
+                with before.each:
+                    self.team = Team.create(name='Blue')
+                    self.response = self.context.client().get(f"/teams/{self.team.id_}")
+                with it('returns 200 ok'):
+                    expect(self.response.status_code).to(equal(200))
+                with it('returns the correct image'):
+                    expect(self.response.json).to(have_key("name", "Blue"))
+                    expect(self.response.json).to(have_key("id_", self.team.id_.urn[9:]))
+            with description('invalid id'):
+                with before.each:
+                    team0 = Team.create(name='Blue')
+                    team1 = Team.create(name="Orange")
+                    team1.delete()
+                    self.response = self.context.client().get(f"/teams/{team1.id_}")
+                with it('responds error 404'):
+                    expect(self.response.status_code).to(equal(404))
