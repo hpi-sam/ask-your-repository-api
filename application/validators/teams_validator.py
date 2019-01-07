9562e29591b0b73e980d8350871d2814a8715765
arne-z <arne.zerndt@student.hpi.de>
2019-01-07 20:37:13 +0100

    Add basic team routes. #32

--- application/validators/teams_validator.py
+++ application/validators/teams_validator.py
@@ -1,6 +1,22 @@
 """ Defines validators for team requests """
 
+from webargs import fields, ValidationError
+
 
 def get_args():
     """Defines and validates params for index"""
     return {}
+
+
+def create_args():
+    """Defines and validates params for create"""
+    return {"name": fields.String(
+        required=True,
+        deserialize=validate_team_name
+    )}
+
+
+def validate_team_name(team_name):
+    if len(team_name) < 1:
+        raise ValidationError("Team name can't be empty")
+    return team_name
