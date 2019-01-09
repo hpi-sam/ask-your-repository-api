123ce2b8d3a10caff1262580712a697de1620e42
Jascha Beste <bestejascha@gmail.com>
2019-01-09 03:59:53 +0100

    Remove unused imports.

--- specs/requests/teams_spec.py
+++ specs/requests/teams_spec.py
@@ -1,16 +1,9 @@
 import sys
-from io import BytesIO
 from flask import current_app
-from mamba import shared_context, included_context, description, context, before, after, it
+from mamba import description, before, after, it
 from expects import *
-from hamcrest import matches_regexp
-from elasticsearch.exceptions import NotFoundError
-from doublex import Mock, Stub, ANY_ARG
 from specs.spec_helpers import Context
 from application.models.team import Team
-from specs.factories.elasticsearch import es_search_response, es_get_response
-from specs.factories.uuid_fixture import get_uuid
-from specs.factories.date_fixture import get_date, date_regex
 
 sys.path.insert(0, 'specs')
 
