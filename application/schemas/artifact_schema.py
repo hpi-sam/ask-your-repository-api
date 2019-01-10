ecf948a499ad2e701ac1c5b1fb624ebf8e8faa59
Arne <arne.zerndt@student.hpi.de>
2019-01-10 13:49:38 +0100

    Initial setup for image recognition. #9

--- application/schemas/artifact_schema.py
+++ application/schemas/artifact_schema.py
@@ -1,11 +1,12 @@
-""" Defines schema for database Artifact objects """
+""" Defines schema for database artifact objects """
 from flask import current_app
 from marshmallow import fields
 from application.base import BaseSchema, output_decorator
+from application.helpers.artifact_helper import build_url
 
 
 class ArtifactSchema(BaseSchema):
-    """ Schema for importing and exporting Artifact objects """
+    """ Schema for importing and exporting artifact objects """
     id = fields.UUID(missing=None)
     type = fields.String(missing="image")
     created_at = fields.DateTime(missing=None)
@@ -19,10 +20,5 @@ class ArtifactSchema(BaseSchema):
     @output_decorator
     def transform_fields(self, data):
         """ Transforms field for output """
-        data["url"] = self.build_url(data.pop("file_url"))
+        data["url"] = build_url(data.pop("file_url"))
         return data
-
-    def build_url(self, file_url):
-        """ Schema: fileserver/id_filename """
-        return current_app.config["FILE_SERVER"] + \
-                "/" + file_url
