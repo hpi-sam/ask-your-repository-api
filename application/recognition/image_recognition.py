db0c7fbb02656e8e87ab4509e4d7365d18023209
arne-z <arne.zerndt@student.hpi.de>
2019-01-11 17:06:21 +0100

    Fixed mocking for Imamge Recognition, Added Unit Tests. #9

--- application/recognition/image_recognition.py
+++ application/recognition/image_recognition.py
@@ -7,15 +7,13 @@ from flask import current_app
 
 def recognize_image(file_url, object_id):
     """Called by classes """
-    spawn_n(_call_google_api, file_url, object_id)
+    spawn_n(_work_asynchronously, file_url, object_id)
 
 
-def _call_google_api(file_url, artifact):
-    """Private method called asynchronously for image recognition."""
-    image_url = build_url(file_url)
-    api_url = current_app.config.CLOUD_VISION_API_URL
-    api_key = current_app.config.CLOUD_VISION_API_KEY
-
+def _call_google_api(url):
+    """Does API call to could vision api"""
+    api_url = current_app.config.get('CLOUD_VISION_API_URL')
+    api_key = current_app.config.get('CLOUD_VISION_API_KEY')
     r = post(url=api_url,
              data={
                  "requests": [
@@ -25,22 +23,26 @@ def _call_google_api(file_url, artifact):
                              {"type": "TEXT_DETECTION"}
                          ],
                          "image": {
-                             "source": {"imageUri": image_url}
+                             "source": {"imageUri": url}
                          }
                      }
                  ]
              },
              params={'key': api_key})
-    if r.status_code is not 200:
-        return
-    body = r.json()
-    labels = body.responses[0].labelAnnotations
+    return r.json()
+
+
+def _work_asynchronously(file_url, artifact):
+    """Private method called asynchronously for image recognition."""
+    image_url = build_url(file_url)
+    res = _call_google_api(image_url)
+    labels = res['responses'][0]['labelAnnotations']
     new_tags = []
     for label in labels:
-        new_tags.append(label.description)
-    texts = body.responses[0].textAnnotations
+        new_tags.append(label['description'])
+    texts = res['responses'][0]['textAnnotations']
     for text in texts:
-        new_tags.append(text.description)
+        new_tags.append(text['description'])
     existing_tags = artifact.tags or []
     tags = existing_tags + new_tags
     artifact.update({"tags": tags})
