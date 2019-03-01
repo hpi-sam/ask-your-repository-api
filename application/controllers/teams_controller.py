38699e214b889c6ea5d9854c4615e304decd5978
luise.benkert <luise.benkert@student.hpi.uni-potsdam.de>
2019-03-01 15:39:02 +0100

    Connect User to Teams.

--- application/controllers/teams_controller.py
+++ application/controllers/teams_controller.py
@@ -4,21 +4,22 @@ Handles all logic of the artefacts api
 from flask import current_app
 from flask_socketio import join_room, leave_room
 from webargs.flaskparser import use_args
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from .application_controller import ApplicationController
 from ..extensions import socketio
-from ..models.team import Team
+from ..models import Team, User
 from ..responders import respond_with
 from ..validators import teams_validator
 
-@socketio.on("JOIN_TEAM")
-def on_team_join(data):
+@socketio.on("ENTER_TEAM_SPACE")
+def on_enter_team_space(data):
     """ Logic for connecting to a Team with socketio """
     current_app.logger.info("Join Room: " + data["team_id"])
     join_room(str(data["team_id"]))
 
-@socketio.on("LEAVE_TEAM")
-def on_team_leave(data):
+@socketio.on("EXIT_TEAM_SPACE")
+def on_exit_team_space(data):
     """ Logic for leaving a Team with socketio """
     current_app.logger.info("Leave Room: " + data["team_id"])
     leave_room(str(data["team_id"]))
@@ -34,16 +35,21 @@ class TeamsController(ApplicationController):
         except Team.DoesNotExist:  # pylint:disable=no-member
             return {"error": "not found"}, 404
 
+    @jwt_required
     @use_args(teams_validator.index_args())
     def index(self, params):  # pylint: disable=W0613
         """Logic for querying several teams"""
-        teams = Team.all()
+        user = User.find_by(id_=get_jwt_identity())
+        teams = list(user.teams)
         return {"teams": respond_with(teams)}, 200
 
+    @jwt_required
     @use_args(teams_validator.create_args())
     def create(self, params):
         """Logic for creating a team"""
         team = Team(name=params["name"]).save()
+        user = User.find_by(id_=get_jwt_identity())
+        team.users.connect(user)
         return respond_with(team), 200
 
     @use_args(teams_validator.update_args())
