cc678cf0bf98385225c8fbbadb1bf763efe4d2e8
Adrian Steppat <adrian.steppat@student.hpi.de>
2019-04-10 18:36:50 +0200

    Add password reset

--- application/extensions.py
+++ application/extensions.py
@@ -1,8 +1,10 @@
 """Extensions module. Each extension is initialized in the app factory located in __init__.py."""
 from flask_socketio import SocketIO
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail
 
 # pylint: disable=invalid-name
 socketio = SocketIO()
 bcrypt = Bcrypt()
+mail = Mail()
 # pylint: enable=invalid-name
--- application/routes.py
+++ application/routes.py
@@ -9,6 +9,7 @@ def create_routes(app):
     from application.users import USERS
     from application.teams import TEAMS
     from application.authentications import AUTHENTICATIONS
+    from application.password_resets import PASSWORD_RESETS
     from application.presentations import PRESENTATIONS
     from application.artifacts.tags.tags_view import TagsView
 
@@ -17,6 +18,7 @@ def create_routes(app):
     app.register_blueprint(TEAMS, url_prefix='/teams')
     app.register_blueprint(AUTHENTICATIONS, url_prefix='/authentications')
     app.register_blueprint(PRESENTATIONS, url_prefix='/presentations')
+    app.register_blueprint(PASSWORD_RESETS, url_prefix='/password_resets')
 
     app.add_url_rule('/tags/suggested', "suggested_tags", TagsView().suggested_tags)
     app.add_url_rule('/', "show_status", HomeView().show_status)
