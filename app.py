03195bac2d60a9e155a00966d89897762958314e
arne-z <arne.zerndt@student.hpi.de>
2019-04-11 18:55:28 +0200

    proposed changes to linting setup.

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from application.extensions import socketio
 
 # Call the Application Factory function to construct a Flask application instance
 # using the standard configuration defined in /instance/flask.cfg
-app = create_app('development_config.cfg')  # pylint: disable=invalid-name
+app = create_app("development_config.cfg")  # pylint: disable=invalid-name
 
 if __name__ != "__main__":
-    gunicorn_logger = logging.getLogger("gunicorn.error")  # pylint: disable=invalid-name
+    gunicorn_logger = logging.getLogger(
+        "gunicorn.error"
+    )  # pylint: disable=invalid-name
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)  # pylint: disable=no-member
 
 if __name__ == "__main__":
-    socketio.run(host='0.0.0.0')  # pylint: disable=no-value-for-parameter
+    socketio.run(host="0.0.0.0")  # pylint: disable=no-value-for-parameter
