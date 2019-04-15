949f7b1043db3cf1963ce4a42bd48c8ec4a7a7a7
Arne <arne.zerndt@student.hpi.de>
2019-04-15 14:22:19 +0200

    Make invite_link shorter than UUID.

--- application/teams/team.py
+++ application/teams/team.py
@@ -1,16 +1,26 @@
 """Access to Team objects in Ne4J"""
-from uuid import uuid4
+import secrets
+import string
 from neomodel import StructuredNode, StringProperty, RelationshipTo, cardinality
 
 from application.model_mixins import DefaultPropertyMixin, DefaultHelperMixin
 from application.teams.team_schema import TeamSchema
 
-class Team(StructuredNode, DefaultPropertyMixin, DefaultHelperMixin):  # pylint:disable=abstract-method
+
+def generate_join_key():
+    while True:
+        key = "".join(secrets.choice(string.ascii_letters + string.digits) for _ in range(8))
+        if not Team.find_by(join_key=key, force=False):
+            break
+    return key
+
+
+class Team(StructuredNode, DefaultPropertyMixin, DefaultHelperMixin):
     """The class that manages Teams"""
 
     schema = TeamSchema
     name = StringProperty(required=True)
-    join_key = StringProperty(required=False, default=uuid4)
+    join_key = StringProperty(required=False, unique_index=True, default=generate_join_key)
 
     artifacts = RelationshipTo("application.models.Artifact", "UPLOADED", cardinality=cardinality.ZeroOrMore)
     members = RelationshipTo("application.models.User", "HAS_MEMBER", cardinality=cardinality.ZeroOrMore)
