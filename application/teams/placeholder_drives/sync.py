d948b03d30d7e0cf5b9200be64d61d95f1824aba
Jascha Beste <bestejascha@gmail.com>
2019-04-23 23:56:00 +0200

    Add uploading and deleting files in gdrive.

--- application/teams/placeholder_drives/sync.py
+++ application/teams/placeholder_drives/sync.py
@@ -1,8 +1,10 @@
 import io
+import json
 import os
 
+import magic
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 from werkzeug.datastructures import FileStorage
 
 from application.artifacts.artifact_creation import ArtifactCreator
@@ -12,15 +14,21 @@ from application.users.oauth.google_oauth import credentials_from_dict
 class DriveAccess:
     drive_folder = "g_drive_downloads"
 
-    def __init__(self, credentials):
+    def __init__(self, credentials, http=None):
         os.makedirs(self.drive_folder, exist_ok=True)
-        self.service = self.drive_service(credentials)
+        self.service = self.drive_service(credentials, http)
 
-    def drive_service(self, credentials):
-        return build("drive", "v3", credentials=credentials)
+    def drive_service(self, credentials, http=None):
+        if http:
+            return build("drive", "v3", http=http)
+        else:
+            return build("drive", "v3", credentials=credentials)
 
     def list_images(self, drive_id):
         result = self.service.files().list(q=f"mimeType contains 'image' and parents='{drive_id}'").execute()
+        with open("drive-list.json", "w") as f:
+            json.dump(result, f)
+
         return result.get("files")
 
     def download_file(self, file_id, filename):
@@ -40,20 +48,30 @@ class DriveAccess:
     def get(self, file_id):
         print(self.service.files().get(file_id).execute())
 
+    def upload_file(self, filename, parent_folder):
+        file_metadata = {'name': filename, 'parents': [parent_folder]}
+        filepath = f"uploads/{filename}"
+        mime = magic.Magic(mime=True)
+        mime_type = mime.from_file(filepath)
+        media = MediaFileUpload(f'uploads/{filename}',
+                                mimetype=mime_type)
+        file = self.service.files().create(body=file_metadata,
+                                           media_body=media,
+                                           fields='id').execute()
+        return file['id']
 
-# Upload File:
-# Upload File
-# Create DriveFile (save ID)
-# Connect to Drive
-
-# Delete remote File
+    def delete_file(self, drive_file_id):
+        try:
+            print(self.service.files().delete(fileId=drive_file_id).execute())
+        except Exception as e:
+            print(e)
 
 
 class ImageSynchronizer:
-    def __init__(self, drive):
+    def __init__(self, drive, http=None):
         self.owner = drive.owner.single()
         self.credentials = credentials_from_dict(self.owner.google_rel.single().credentials)
-        self.drive_access = DriveAccess(self.credentials)
+        self.drive_access = DriveAccess(self.credentials, http)
         self.drive = drive
         self.team = drive.team.single()
         if drive.page_token is None:
@@ -103,9 +121,13 @@ class ImageSynchronizer:
         self.drive.save()
 
     def upload_all(self):
-        # TODO: For all artifacts without DriveFile:
-        # TODO: Upload to GDRIVE
-        pass
+        artifacts = self.drive.team.single().artifacts
+        for artifact in artifacts:
+            if artifact in self.drive.files:
+                return
+            id = self.drive_access.upload_file(artifact.file_url, self.drive.drive_id)
+            self.drive.files.connect(artifact, {"gdrive_file_id": id})
+
 
     # TODO: Local changes vs remote changes (other changes than add/delete?)
 
@@ -125,6 +147,14 @@ class ImageSynchronizer:
             if "newStartPageToken" in response:
                 self.drive.page_token = response.get("newStartPageToken")
                 self.drive.save()
+
+    def delete_all(self):
+        artifacts = self.drive.files
+        for artifact in artifacts:
+            rel = self.drive.files.relationship(artifact)
+            print(rel.gdrive_file_id)
+            self.drive_access.delete_file(rel.gdrive_file_id)
+            self.drive.files.disconnect(artifact)
 
     def initialize_sync(self):
         self.download_all()
