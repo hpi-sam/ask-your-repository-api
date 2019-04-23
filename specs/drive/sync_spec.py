a28dcb727f54efb868bf2261f372f32a509f8a62
Jascha Beste <bestejascha@gmail.com>
2019-04-23 23:56:00 +0200

    Add uploading and deleting files in gdrive.

--- specs/drive/sync_spec.py
+++ specs/drive/sync_spec.py
@@ -1,14 +1,31 @@
+from googleapiclient.http import HttpMockSequence
 from mamba import description, before, it
 
 from application.teams.placeholder_drives.drive import Drive
-from application.teams.placeholder_drives.sync import DriveAccess, ImageSynchronizer
-from application.users.oauth.google_oauth import credentials_from_dict
+from application.teams.placeholder_drives.sync import ImageSynchronizer
+from specs.factories.image_recognition import mock_image_recognition
 from specs.spec_helpers import Context
 
 with description("drive") as self:
     with before.each:
         self.context = Context()
 
+
+    # generating discovery file:
+    #        with open("drive-discovery.json", "w") as f:
+    #            f.write(_retrieve_discovery_doc("https://www.googleapis.com/discovery/v1/apis/drive/v3/rest", build_http(), cache_discovery=False))
+    def file_content(path):
+        return open(path).read()
+
+
     with description("downloading"):
         with it("downloads something"):
-            ImageSynchronizer(Drive.all()[3]).download_all()
+            with mock_image_recognition:
+                # http = HttpMock('drive-discovery.json', {'status': '200'})
+                #http = HttpMockSequence([({'status': 200}, file_content('drive-discovery.json')),
+                #                         ({'status': 200}, file_content('drive-list.json'))])
+                api_key = 'your_api_key'
+                sync = ImageSynchronizer(Drive.all()[3])
+                print(sync.delete_all())
+
+    #
