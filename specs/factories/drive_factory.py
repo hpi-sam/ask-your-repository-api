568b13df9d332256a225b6ba1803edd4d21ff41c
Jascha Beste <bestejascha@gmail.com>
2019-04-29 12:53:13 +0200

    Add drive factory in tests.

---
+++ specs/factories/drive_factory.py
@@ -0,0 +1,21 @@
+from application.teams.placeholder_drives.drive import Drive
+from .google_oauth_factory import GoogleOAuthFactory, google_credentials
+from .team_factory import TeamFactory
+
+
+class DriveFactory:
+    @classmethod
+    def create_drive(cls, user=None, team=None):
+        drive = Drive(drive_id="1HTeLgrLX3L3QvvTd7Q6f9Sbaalvq4HU0").save()
+        if user:
+            drive.owner.connect(user)
+        else:
+            user = GoogleOAuthFactory.create_user_with_google(credentials=google_credentials)
+            drive.owner.connect(user)
+        if team:
+            drive.team.connect(team)
+        else:
+            team = TeamFactory.create_team(members=[user])
+            drive.team.connect(team)
+        drive.save()
+        return drive
