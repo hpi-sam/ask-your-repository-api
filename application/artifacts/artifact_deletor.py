229d56fea8287c13a7792822478c43e51b457242
Jascha Beste <bestejascha@gmail.com>
2019-05-03 15:28:05 +0200

    Move file deletion to ArtifactDeletor.

--- application/artifacts/artifact_deletor.py
+++ application/artifacts/artifact_deletor.py
@@ -1,5 +1,8 @@
 from .elastic.elastic_syncer import ElasticSyncer
+import os
 
+from pathlib import Path
+from flask import current_app
 
 class ArtifactDeletor:
     def __init__(self, artifact):
@@ -7,3 +10,10 @@ class ArtifactDeletor:
 
     def delete(self):
         ElasticSyncer.for_artifact(self.artifact).delete()
+        filename_without_ext = os.path.splitext(self.artifact.file_url)[0]
+        self._delete_files_starting_with(filename_without_ext)
+
+
+    def _delete_files_starting_with(self, start_string):
+        for p in Path(current_app.config["UPLOAD_FOLDER"]).glob(f"{start_string}*"):
+            p.unlink()
